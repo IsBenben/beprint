c30a9a6b93587f09f093db9efdeaa9f65fc66f96
IsBenben <542863140@qq.com>
2024-09-16 22:30:24 +0800

    Change an example

--- examples/Columns Layout.py
+++ examples/Columns Layout.py
@@ -2,10 +2,11 @@ from beprint import *
 
 clear_screen()
 
-# 2 Columns Layout
-result = columns(3, 3, width - 4, height - 4, sizes=[0.2, 0.8], callbacks=[
+# Columns Layout
+result = columns(3, 3, width - 4, height - 4, sizes=[0.1, 0.3, 0.6], callbacks=[
     lambda _, *args: border(Chars(style_1), *args, title='Info'),
     lambda _, *args: border(Chars(style_1), *args, title='Contents'),
+    lambda _, *args: border(Chars(style_1), *args, title='Code'),
 ])
 result[0].print('Powered by BePrint')
 beprint({
@@ -17,3 +18,8 @@ beprint({
     'array': [1, 2, 3],
     'object': object()
 }, result[1])
+with open(__file__, 'r') as f:
+    result[2].print(highlight_code(f.read(), 'python'))
+
+write_on(height, 1, 'Press enter to exit...')
+input()
